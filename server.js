554beb13beb38b82671f8c03bc8582eb08afda9e
quangChe <quang.n.che@gmail.com>
2019-05-08 14:40:40 -0700

    Serving Files and Yelp Data
    Removed cors package and set res headers manually. Also testing new
    approach to provide querying Yelp API from the front end app. Lastly,
    modified configurations to serve the built React app from root path of
    the API (this may not work later when setting up React Router).

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 require('dotenv').config();
-const app = require('express')();
-const cors = require('cors');
+const express = require('express')
+const app = express();
 const axios = require('axios');
 const YelpKey = process.env.YELP_API_KEY;
+const path = require('path');
+const rootDir = (file='') => path.join(__dirname, '..', 'app', 'build', file);
 
-app.get('/', cors(), async (req, res) => {
-    const url = 'https://api.yelp.com/v3/businesses/search?term=happy+hour&location=1131+wass+st';
-    const headers = {
-      'Authorization': YelpKey,
-    }
-    const yelp = await axios.get(url, {headers});
-    res.status(200).send(yelp.data.businesses);
+app.use(express.static(rootDir()));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
 })
 
-app.use((req, res, next) => {
-  cors({origin: 'http://localhost:9000'});
-  next();
+app.get('/list/businesses', async (req, res) => {
+  const url = `https://api.yelp.com/v3/businesses/search?${req.query.search}`;
+  const headers = {'Authorization': YelpKey};
+  const yelp = await axios.get(url, {headers});
+  res.status(200).send(yelp.data.businesses);
+})
+
+app.get('/*', async (req, res) => {
+  res.status(200).sendFile(rootDir('index.html'))
+})
+
+app.use('*', (req, res) => {
+  res.status(400).send('Bad Request');
 })
 
 app.listen(9000, () => console.log('Now listening on port 9000.'))
