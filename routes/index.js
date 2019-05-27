6be3997a229869b3a3d504722719ab87af860a0e
quangChe <quang.n.che@gmail.com>
2019-05-27 12:48:04 -0700

    Refactor to Business Controllers
    Cleaned up router code into business controllers.

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,7 @@
-const YelpKey = process.env.YELP_API_KEY;
 const router = require('express').Router();
-const axios = require('axios');
+const businesses = require('../controllers/businesses');
 
-router.get('/businesses/search', async (req, res) => {
-  if (!req.query.q) {
-    return res.status(400).send('Invalid query provided.');
-  }
-  
-  try {
-    const url = `https://api.yelp.com/v3/businesses/search?${req.query.q}`;
-    const headers = {'Authorization': YelpKey};
-    const yelp = await axios.get(url, {headers});
-    res.status(200).send(yelp.data.businesses);
-  } catch (err) {
-    console.error(err);
-    const status = err.response.status || 500;
-    const message = err.response.data.error.description || 'Unexpected error';
-    res.status(status).send(message);
-  }
-})
-
-router.get('/businesses/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const url = `https://api.yelp.com/v3/businesses/${id}`;
-    const headers = {'Authorization': YelpKey};
-    const yelp = await axios.get(url, {headers});
-    return res.status(200).send(yelp.data);
-  } catch(err) {
-    const status = err.response.status || 500;
-    const message = err.response.data.error.description || 'Unexpected error';
-    return res.status(status).send(message);
-  }
-})
+router.get('/businesses/search', businesses.search);
+router.get('/businesses/:id', businesses.getById);
 
 module.exports = router;
