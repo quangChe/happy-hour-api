9c138459bf8455a5d68b96f27146ae1beb09b94f
quangChe <quang.n.che@gmail.com>
2019-07-20 09:12:47 -0700

    Logs and cleanup
    Added morgan for server logging and cleaned up code in app's index file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const env = require('dotenv').config();
 const path = require('path');
+const morgan = require('morgan');
 const schema = require('./schema');
 const express = require('express');
 const routeHandler = require('./routes');
@@ -31,25 +32,22 @@ const app = express();
 // });
 // db.end();
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
+const headers = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
-})
+}
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(headers);
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+
 
 app.use('/api', routeHandler);
-app.use('/graphql', expressGraphQL({
-  graphiql: true,
-  schema,
-}))
-
-app.use('*', (req, res) => {
-  res.status(400).send('Bad Request');
-})
+app.use('/graphql', expressGraphQL({ graphiql: true, schema }));
+app.use('*', (req, res) => res.status(400).send('Bad Request'));
 
 app.listen(9000, process.env.HOSTNAME,
   () => console.log('Now listening on port 9000.'))
