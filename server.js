93ff54562f43c12efc7381ebfc8d0c6df97c5c21
quangChe <quang.n.che@gmail.com>
2019-07-19 22:58:48 -0700

    Removed default MongoDB connection behavior

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 
 // MongoDB Startup:
 // -------------------
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true })
-const Test = mongoose.model('Test', new mongoose.Schema({name: String}));
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true })
+// const Test = mongoose.model('Test', new mongoose.Schema({name: String}));
 
 // MySQL Startup:
 // --------------
