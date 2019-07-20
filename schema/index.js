3d8fcb18b4dab34ed7c98c353340f9db61bba7b1
quangChe <quang.n.che@gmail.com>
2019-07-20 09:13:56 -0700

    JSON Server
    Implement json-server to mock an external document API that is being
    used by GraphQL query.

--- schema/index.js
+++ schema/index.js
@@ -1,18 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const { 
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
 } = graphql;
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 29 },
-  { id: '47', firstName: 'Samantha', age: 28 },
-  { id: '4', firstName: 'John', age: 24 },
-  { id: '47', firstName: 'Roland', age: 32 }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -30,7 +23,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data)
       }
     }
   }
