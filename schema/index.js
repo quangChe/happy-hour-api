cddc96501db3ce74c917946ebb3de8993b45c713
quangChe <quang.n.che@gmail.com>
2019-07-21 12:44:57 -0700

    Built out the FavoriteType and mapped it to the UserType schema to be popopulated when a user item is retrieved with GraphQL

--- schema/index.js
+++ schema/index.js
@@ -7,13 +7,28 @@ const {
   GraphQLSchema,
 } = graphql;
 
+const FavoriteType = new GraphQLObjectType({
+  name: 'Favorite',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }
+})
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString},
     firstName: { type: GraphQLString},
     age: { type: GraphQLInt},
-  }
+    favorite: { 
+      type: FavoriteType,
+      resolve(parentVal, args) {
+        return axios.get(`http://localhost:3000/favorites/${parentVal.favoriteId}`)
+          .then(resp => resp.data)
+      }
+    }
+  },
 });
 
 const RootQuery = new GraphQLObjectType({
